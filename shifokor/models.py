10303752c5a426b0c1cc34664b20281276cdab45
asca04 <askarbekyusufboyev@gmail.com>
2025-03-17 19:06:36 +0500

    shifokorlar

--- shifokor/models.py
+++ shifokor/models.py
@@ -1,31 +1,30 @@
-from django.db.models import Model
-from rest_framework.fields import CharField, DateField, IntegerField
+from django.db.models import Model, CharField, IntegerField, DateField, OneToOneField, CASCADE
+
+
+
+class ShifokorQoshish(Model):
+
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+    ]
+
+    jshshir = CharField(max_length=14)
+    familya = CharField(max_length=50)
+    ismi = CharField(max_length=50)
+    otasining_ismi = CharField(max_length=50)
+    jinsi = CharField(max_length=1, choices=GENDER_CHOICES)
+    tugilgan_sana = DateField()
+
+    def __str__(self):
+        return f"{self.ismi} {self.familya} - {self.jshshir}"
+
 
 class Shifokorlar(Model):
-    fio = CharField(max_length=150)
-    tugilgan_sana = DateField(allow_null=False)
+    shifokor = OneToOneField('ShifokorQoshish', CASCADE, related_name='shifokor')
     lavozimi = CharField(max_length=100)
     mutaxasislik_toifasi = CharField(max_length=100)
     telefon_raqami = CharField(max_length=13)
-    biriktirilgan_muassasa = CharField(allow_null=True)
-    jshshir = IntegerField(max_value=99999999999999)
+    biriktirilgan_muassasa = CharField(null=True)
     ish_staji = IntegerField()
     oxirgi_malaka_oshirgan_joyi = CharField(max_length=150)
-
-    def save(
-        self,
-        *args,
-        force_insert=False,
-        force_update=False,
-        using=None,
-        update_fields=None,
-    ):
-        a = self.user.name
-        obj = self.object
-        obj.mutaxasislik_toifasi = a
-        obj.save(self, *args,
-        force_insert=False,
-        force_update=False,
-        using=None,
-        update_fields=None,)
-
--- shifokor/views.py
+++ shifokor/views.py
@@ -1,10 +1,45 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from shifokor.models import Shifokorlar, ShifokorQoshish
+from .serializers import ShifokorModelSerializer
+from shifokor.serializers import ShifokorQoshishModelSerializer
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from rest_framework.permissions import AllowAny
 
-from shifokor.models import Shifokorlar
-from serializers import ShifokorModelSerializer
 
 class ShifokorModelViewSet(ModelViewSet):
     queryset = Shifokorlar.objects.all()
     serializer_class = ShifokorModelSerializer
+    permission_classes = (AllowAny,)
 
 
+class Shifokor_qoshish(CreateAPIView):
+    queryset = ShifokorQoshish
+    serializer_class = ShifokorQoshishModelSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data = request.data)
+        if serializer.is_valid():
+            shifokor = self.queryset.objects.get(jshshir=request.data.get('jshshir'))
+            print(shifokor)
+            return Response(
+            {
+                    "shifokor": {
+                        "jshshir": shifokor.jshshir,
+                        "ism": shifokor.ismi,
+                        "familiya": shifokor.familya,
+                        "tugilgan_sana": shifokor.tugilgan_sana,
+                        "jinsi": shifokor.jinsi,
+                    }
+                },
+                status=HTTP_200_OK
+            )
+
+        return Response(
+            {
+                "message": "Xatolik yuz berdi!",
+                "errors": serializer.errors
+            },
+            status=HTTP_400_BAD_REQUEST
+        )
