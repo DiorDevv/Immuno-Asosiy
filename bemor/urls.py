1e9a775e6ba380767b984eef21a3aca816c92297
Diyorbek <diyorbekaxmadjonov98@gamil.com>
2025-03-19 19:59:55 +0500

    pdf qo'shildi

--- bemor/urls.py
+++ bemor/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BemorQoshishCreateView, ManzilViewSet, OperatsiyaBolganJoyViewSet, \
-    BemorViewSet, ExportBemorExcelView, BemorHolatiStatistika
+    BemorViewSet, ExportBemorExcelView, BemorHolatiStatistika, BemorPDFDownloadView
 
 # ViewSet-lar uchun Router yaratamiz
 router = DefaultRouter()
@@ -13,6 +13,7 @@ urlpatterns = [
     path('bemor-qoshish/', BemorQoshishCreateView.as_view(), name='bemor-qoshish'),
     path("export/bemorlar/", ExportBemorExcelView.as_view(), name="export_bemorlar"),
     path('bemor-statistika/', BemorHolatiStatistika.as_view(), name='bemor-holati-statistika'),
+    path('bemor/<int:pk>/pdf/', BemorPDFDownloadView.as_view(), name='bemor-pdf-download'),
 
     # ManzilViewSet API-lari uchun avtomatik URL-larni qo‘shamiz
     path('', include(router.urls)),
